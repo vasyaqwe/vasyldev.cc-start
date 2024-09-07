ecba043d81a09b50b6a1477dccddcf1045cc580d
vasyaqwe <elekton.fafik@gmail.com>
2024-09-07 18:25:27 +0300

    deploy

--- app/routes/hello.tsx
+++ app/routes/hello.tsx
@@ -19,6 +19,7 @@ export const Route = createFileRoute("/hello")({
          { name: "googlebot", content: "index, follow" },
       ]
    },
+   pendingComponent: () => <div>Loading...</div>,
 })
 
 function Component() {
