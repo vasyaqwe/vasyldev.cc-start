4670b27c8a250fc965fbf82b277f9835eb7543a2
vasyaqwe <elekton.fafik@gmail.com>
2024-09-07 19:31:06 +0300

    deploy

--- app/routes/hello2/index.tsx
+++ app/routes/hello2/index.tsx
@@ -2,4 +2,7 @@ import { createFileRoute } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/hello2/")({
    component: () => <div>Hello /hello2/!</div>,
+   loader: async () => {
+      return "OK"
+   },
 })
